b5295829e1861326058abf208d902a5bf7249a23
SerhiiKushanrenko <Serhii.Kushnarenko@fivesysdev.com>
2022-12-02 11:27:37 +0200

    Fix Bags

--- src/App.js
+++ src/App.js
@@ -1,76 +1,72 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 import LoginButton from "./Components/LoginButton";
 import CircularIndeterminate from "./Components/Progress";
-import { createTheme, ThemeProvider } from "@mui/system";
-import { ThemeContext } from "@emotion/react";
-import { isDisabled } from "@testing-library/user-event/dist/utils";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
-import { useFormControl } from '@mui/material/FormControl';
-import { Input } from "@mui/material";
+import { v4 as uuidv4 } from 'uuid';
+import { HttpTransportType, HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [guid, setGuid] = useState();
   const [visible,setVisible] = useState(false)
+  var ourGuid = uuidv4();
   
-
   const URL = "https://localhost:7241/api/User";
   const URLCLOUD = "https://localhost:7079/checkHub";
   const URLPOSTCRED = "https://localhost:7079/api/AddGuid"
-   
-
-  function test12 (){ 
-    axios.get("https://localhost:7079/api/UserInfo").then((resp) => {
-    console.log(resp);
-  });}
   
 
-  function startLisenBack() {
-    const hubConnection = new HubConnectionBuilder()
-    .withUrl(URLCLOUD)
-    .build()
-   
-    hubConnection.start()
-    console.log(hubConnection);
-    sendData(hubConnection)
 
-    hubConnection.on("CheckHubAsync", (result) =>{
-      console.log(result);
-       setVisible(result)
-    })
-    hubConnection.stop()
-  } 
+  const signalR = () => {
+    const connection = new HubConnectionBuilder()
+      .withUrl("https://localhost:7079/checkHub")
+      .build();
 
-  function sendData (hubConnection) 
+      connection.start().then( () => { sendData(connection.connection.connectionId);})
+
+      console.log(connection);
+
+      connection.on("Notify", (result) =>{
+        console.log(result);
+         setVisible(result)
+      })
+
+      // if (visible === true) {
+      //   setIsLoading(true)
+      // }
+      
+  };
+
+  function sendData (connectionId) 
   {
-    
+    console.log(guid);
      axios.post(URLPOSTCRED,{
-      guid : guid,
-      connectionId : hubConnection.connectionId
+      id : ourGuid,
+      connectionId : connectionId
      })
      
   }
 
 
 
-  function test() {
+  function Start() {
+   
     setIsLoading(true);
-    axios.get(URL).then((resp) => {
-      setGuid(resp.data);
-      setIsLoading(false);
-      //test12();
-      startLisenBack();
-      
-    });
+    axios.post(URL,
+      {id:ourGuid}
+    )
+    signalR();
   }
 
+  
+  
 
   return (
     <div className="App">
-      <LoginButton onClick={test}>fdsfds</LoginButton>
+      <LoginButton onClick={Start}>fdsfds</LoginButton>
       {isLoading ? <CircularIndeterminate /> : <p>{guid}</p>}
       {visible? <p>Welcome</p>: null}
     </div>
